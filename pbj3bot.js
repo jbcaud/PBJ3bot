ed03dc0a8e64b20972a87142eed41d3a9ef0ce6a
jbcaud <jbcaud@gmail.com>
2020-12-26 00:01:46 -0500

    Added timed message functionality, as well as a couple basic commands.

--- pbj3bot.js
+++ pbj3bot.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 // Define configuration options
+//obtained from env file
 const opts = {
   identity: {
     username: process.env.user,
@@ -23,6 +24,13 @@ client.on('connected', onConnectedHandler);
 // Connect to Twitch:
 client.connect();
 
+//instantiate variables to be used
+var time = new Date().getTime();
+var min = 1000 * 60, timetr = 0;
+
+//send the timed messages every 15 minutes
+setInterval(timedMessage, 15 * min);
+
 // Called every time a message comes in
 function onMessageHandler (target, context, msg, self) {
   if (self) { return; } // Ignore messages from the bot
@@ -35,10 +43,32 @@ function onMessageHandler (target, context, msg, self) {
     const num = rollDice();
     client.say(target, `You rolled a ${num}`);
     console.log(`* Executed ${commandName} command`);
-  } else {
+  }//end of if
+  else if (commandName === '!lurk') {
+    client.say(opts.channels[0], context.username + ` is now lurking. Thanks for being here, I hope you stay cozy! peepoBlanket`);
+    console.log(`* Executed ${commandName} command`);
+  }//end of if
+  //unknown command 
+  else{
     console.log(`* Unknown command ${commandName}`);
-  }
-}
+  }//end of else
+}//end of onMessageHandler
+
+//Called at selected intervals of time
+function timedMessage(){
+  //first message (follow)
+  if (timetr == 0){
+    //output message and change track var
+    client.say(opts.channels[0], 'If you are enjoying the stream, follow us so you can know when we go live!');
+    timetr = 1;
+  }//end of if
+  //second message (points)
+  else{
+    //output message and change track var
+    client.say(opts.channels[0], 'This channel uses PBJ Points! You earn PBJ Points for watching, following, and other actions. Use !pbj to learn more!');
+    timetr = 0;
+  }//end of else
+}//end of timedMessage
 
 // Function called when the "dice" command is issued
 function rollDice () {
